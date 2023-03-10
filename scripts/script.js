6913e417a77461e71b1fe0136ed5cbc881d20c7a
Daria Rutts <darya.rutts@yandex.ru>
2023-03-10 19:50:09 +0300

    refactor: вынести initialCards в отдельный файл

--- scripts/script.js
+++ scripts/script.js
@@ -23,33 +23,6 @@ const popupImage = imagePopup.querySelector('.popup__image')
 const popupCaption = imagePopup.querySelector('.popup__caption')
 const closeImagePopupButton = imagePopup.querySelector('.button_action_close')
 
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 function popupOpen(popup) {
     popup.classList.add('popup_opened')
 }
@@ -152,5 +125,3 @@ closeAddPopupButton.addEventListener('click', () => {
 })
 
 addForm.addEventListener('submit', handlerAddCardFormSubmit)
-
-initialCards.forEach(addCard)
