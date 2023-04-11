64f7ca44d874df7041aeeb93ea22fd4ff07705c0
Daria Rutts <darya.rutts@yandex.ru>
2023-04-11 19:47:07 +0300

    refactor: создать класс Section, который отвечает за отрисовку элементов

---
+++ scripts/Section.js
@@ -0,0 +1,21 @@
+export class Section {
+  #container
+  #renderedItems
+  #renderer
+
+  constructor({ items, renderer }, containerSelector) {
+    this.#renderedItems = items
+    this.#renderer = renderer
+    this.#container = document.querySelector(containerSelector)
+  }
+
+  renderItems() {
+    this.#renderedItems.forEach((item) => {
+      this.#renderer(item)
+    })
+  }
+
+  addItem(element) {
+    this.#container.append(element)
+  }
+}
