96e94897743269239d9dec1221f2651ca764fe4e
Daria Rutts <darya.rutts@yandex.ru>
2023-04-11 18:37:42 +0300

    refactor: создать класс Popup, который отвечает за открытие и закрытие попапа

---
+++ scripts/Popup.js
@@ -0,0 +1,36 @@
+export class Popup {
+  #popup
+  #closeButton
+
+  constructor(popupSelector) {
+    this.#popup = document.querySelector(popupSelector)
+    this.#closeButton = this.#popup.querySelector('.button_action_close')
+  }
+
+  open() {
+    this.#popup.classList.add('popup_opened')
+    document.addEventListener('keydown', this.#handleEscClose())
+  }
+
+  close() {
+    this.#popup.classList.remove('popup_opened')
+    document.removeEventListener('keydown', this.#handleEscClose())
+  }
+
+  #handleEscClose(evt) {
+    if(evt.key === 'Escape') {
+      this.close()
+    }
+  }
+
+  #handleOverlayClose(evt) {
+    if(evt.target.classList.contains('popup_opened')) {
+      this.close()
+    }
+  }
+
+  setEventListeners() {
+    this.#closeButton.addEventListener('click', this.close())
+    document.addEventListener('mousedown', this.#handleOverlayClose())
+  }
+}
